b1198ed1fceda0236a706d07360382b5a1ada8c5
Francis Lussier <daesolus6@gmail.com>
2014-10-05 17:43:00 -0400

    Update Armus.cpp

--- Armus.cpp
+++ Armus.cpp
@@ -1,40 +1,49 @@
 /*
- * Programme Armus - Équipe P24
- * Création : 01/10/2014
- * Modifié  : 02/10/2014
+ * Programme Armus - Equipe P24
+ * Creation : 01/10/2014
+ * Modifie  : 04/10/2014
  */
 #include <iostream>
 #include <libarmus.h>
 #include <cmath>
 #include "Armus.h"
+
 int main()
 {
-	const short int VITESSE_INITIALE = 50;
-	const short int TRANSITIONS = 20;
+	const short int TRANSITIONS = 16;
 	const short int MODE = 1; // TEST:0 DEV:1
 
 	int i = 1; //nb de cycle**
 
 	robot unRobot;
-	unRobot.vitesseDroit = VITESSE_INITIALE;
-	unRobot.vitesseGauche = VITESSE_INITIALE+2;
+	unRobot.vitesseGauche = 80;
+	unRobot.vitesseDroit = 78;
 	unRobot.distanceDroit = 0;
 	unRobot.distanceGauche = 0;
+	unRobot.totalDistanceDroit = 0;
+	unRobot.totalDistanceGauche = 0;
+
+
+	int init;
+	//Pour initialiser les compteurs avant le début, évite quelques bogues intermittants.
+	init=ENCODER_Read(ENCODER_RIGHT);
+	init=ENCODER_Read(ENCODER_LEFT);
 
     LCD_ClearAndPrint("New\n");
-    while(true)
-    {
+
     	if(MODE == 0)
     	{
+    	 while(true)
+    	 {
     	    setVitesseTEST(unRobot,i,TRANSITIONS);
+    	    i++;
+    	 }
     	}
     	else if(MODE == 1)
     	{
-    		setVitesse(unRobot,i,TRANSITIONS);
+    	    parcours(unRobot,i,TRANSITIONS);
     	}
-        i++;
-    }
+
+
     return 0;
 }
-
-
