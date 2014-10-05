88e939608c492292950719cdaae3653738cdf132
Francis Lussier <daesolus6@gmail.com>
2014-10-05 17:42:44 -0400

    Update Armus.h

--- Armus.h
+++ Armus.h
@@ -3,19 +3,27 @@
  * Création : 01/10/2014
  * Modifié  : 02/10/2014
  */
+
+
 struct robot
 {
     int vitesseDroit;
 		int vitesseGauche;
-		unsigned short encodeurDroit;
-		unsigned short int encodeurGauche;
+		short int encodeurDroit;
+		short int encodeurGauche;
 		int distanceDroit;
 		int distanceGauche;
 		int totalDistanceDroit;
 		int totalDistanceGauche;
+		int currentDistanceDroit;
+		int currentDistanceGauche;
 };
 void setVitesse(robot&,int&,const short int);
 int distance();
 
 //TEST
 void setVitesseTEST(robot&,int&,const short int);
+
+void cmToRevolution(int cm,robot &unRobot,int &i,const short int TRANSITIONS);
+void parcours(robot &unRobot,int &i,const short int TRANSITIONS);
+void setAngle(int angle,robot &unRobot,int &i,const short int TRANSITIONS);
