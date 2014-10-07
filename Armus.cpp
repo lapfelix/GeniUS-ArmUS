9bee89d9bc6e48c4766ac124cda2ac6d0127fcf5
Felix Lapalme <lap.felix@gmail.com>
2014-10-07 13:11:48 -0400

    Modifications à Pier-Luc
    “Vrai bon programme” - Pier-Luc Coté

--- Armus.cpp
+++ Armus.cpp
@@ -1,49 +1,58 @@
 /*
- * Programme Armus - Equipe P24
- * Creation : 01/10/2014
- * Modifie  : 04/10/2014
- */
-#include <iostream>
+============================================================================
+ Name : Armus.cpp
+ Author :Pier-Luc
+ Version :10.0
+ Description : Hello world - Exe source file
+============================================================================
+*/
+
+// Include Files
+
 #include <libarmus.h>
-#include <cmath>
-#include "Armus.h"
+
+// Global Functions
+const int TEMPS = 100;//msecondes
+const float FC_VITESSE = 1;//facteur de correction de la vitesse
+const float FC_DISTANCE = 0.5;//facteur de correction de la distance
 
 int main()
 {
-	const short int TRANSITIONS = 16;
-	const short int MODE = 1; // TEST:0 DEV:1
+	int consigneVitesse = 10;//transitions / valeur de temps
+	int distanceVoulue = 0; // distance suppos�ment parcourue par le robot;
+	int init = ENCODER_Read(ENCODER_RIGHT);
+	init = ENCODER_Read(ENCODER_LEFT);
+	int lecturevitesseDroite = 0;
+	int lecturevitesseGauche = 0;
+	int vitesseMoteurDroit = 0;
+	int vitesseMoteurGauche = 0;
+	int distanceMoteurDroit=0;
+	int distanceMoteurGauche=0;
 
-	int i = 1; //nb de cycle**
-
-	robot unRobot;
-	unRobot.vitesseGauche = 80;
-	unRobot.vitesseDroit = 78;
-	unRobot.distanceDroit = 0;
-	unRobot.distanceGauche = 0;
-	unRobot.totalDistanceDroit = 0;
-	unRobot.totalDistanceGauche = 0;
-
-
-	int init;
-	//Pour initialiser les compteurs avant le début, évite quelques bogues intermittants.
-	init=ENCODER_Read(ENCODER_RIGHT);
-	init=ENCODER_Read(ENCODER_LEFT);
-
-    LCD_ClearAndPrint("New\n");
-
-    	if(MODE == 0)
-    	{
-    	 while(true)
-    	 {
-    	    setVitesseTEST(unRobot,i,TRANSITIONS);
-    	    i++;
-    	 }
-    	}
-    	else if(MODE == 1)
-    	{
-    	    parcours(unRobot,i,TRANSITIONS);
-    	}
+	while(1)
+	{
+	THREAD_MSleep(TEMPS);
+	lecturevitesseDroite = ENCODER_Read(ENCODER_RIGHT);
+	lecturevitesseGauche = ENCODER_Read(ENCODER_LEFT);
+	distanceVoulue= distanceVoulue+consigneVitesse;
+	distanceMoteurDroit = distanceMoteurDroit+lecturevitesseDroite;
+	distanceMoteurGauche = distanceMoteurGauche + lecturevitesseGauche;
 
 
-    return 0;
+	vitesseMoteurDroit = -FC_VITESSE*(lecturevitesseDroite-consigneVitesse)-FC_DISTANCE*(distanceMoteurDroit-distanceVoulue);
+	vitesseMoteurGauche = -FC_VITESSE*(lecturevitesseGauche-consigneVitesse)-FC_DISTANCE*(distanceMoteurGauche-distanceVoulue);
+	LCD_Printf(",EG %d, ED %d\n ", lecturevitesseGauche-consigneVitesse, lecturevitesseDroite-consigneVitesse);
+
+
+
+	MOTOR_SetSpeed(7,vitesseMoteurGauche);
+	MOTOR_SetSpeed(8,vitesseMoteurDroit);
+
+//allo
+
+
+
+	}
+
+	return 0;
 }
