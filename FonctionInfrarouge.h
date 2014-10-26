52475e5d064802b9355752ca729a2cb501cbb9f3
Stachewill <William.Grand-Maison@Usherbrooke.ca>
2014-10-26 19:00:10 -0400

    header du fichier FonctionInfrarouge.cpp

---
+++ FonctionInfrarouge.h
@@ -0,0 +1,18 @@
+/*
+ * FonctionInfrarougw.h
+ *
+ *  Created on: 2014-10-26
+ *      Author: graw2302
+ */
+
+#ifndef FONCTIONINFRAROUGW_H_
+#define FONCTIONINFRAROUGW_H_
+
+
+
+
+#endif /* FONCTIONINFRAROUGW_H_ */
+
+void testInfrarouge();
+int LecturePin();
+int CalculDistance(int);
